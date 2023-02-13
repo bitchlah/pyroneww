59b3d8c2e8355520c83c78f0796887e912928b67
bitchlah <106461653+bitchlah@users.noreply.github.com>
2023-02-13 11:37:09 +0700

    Update updater.py

--- Kazu/modules/updater.py
+++ Kazu/modules/updater.py
@@ -175,7 +175,7 @@ async def upstream(client: Client, message: Message):
             repo.__del__()
             return
         await status.edit(
-            "`[HEROKU]: Update Deploy PyroZu-Userbot Sedang Dalam Proses...`"
+            "`[HEROKU]: Update Deploy ALBY-PYROBOT Sedang Dalam Proses...`"
         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
@@ -192,7 +192,7 @@ async def upstream(client: Client, message: Message):
         except GitCommandError:
             pass
         await status.edit(
-            "`PyroZu-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`"
+            "`ALBY-PYROBOT Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`"
         )
     else:
         try:
@@ -201,7 +201,7 @@ async def upstream(client: Client, message: Message):
             repo.git.reset("--hard", "FETCH_HEAD")
         await updateme_requirements()
         await status.edit(
-            "`PyroZu-Userbot Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
+            "`ALBY-PYROBOT Berhasil Diupdate! Userbot bisa di Gunakan Lagi.`",
         )
         args = [sys.executable, "-m", "Kazu"]
         execle(sys.executable, *args, environ)
@@ -270,7 +270,7 @@ async def updaterman(client: Client, message: Message):
 add_command_help(
     "update",
     [
-        ["update", "Untuk melihat list pembaruan terbaru dari PyroZu-Userbot."],
+        ["update", "Untuk melihat list pembaruan terbaru dari ALBY-PYROBOT."],
         ["update deploy", "Untuk mengupdate userbot."],
     ],
 )
