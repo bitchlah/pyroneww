f1b4e0e651da70c8d9f0efb053c1acc510a9fe08
bitchlah <106461653+bitchlah@users.noreply.github.com>
2023-02-13 09:59:58 +0700

    Update bot.py

--- Kazu/modules/bot/bot.py
+++ Kazu/modules/bot/bot.py
@@ -58,6 +58,7 @@ async def _callbacks(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("ub_modul_(.*)"))
+@cb_wrapper
 async def on_plug_in_cb(_, callback_query: CallbackQuery):
     modul_name = callback_query.matches[0].group(1)
     commands: dict = CMD_HELP[modul_name]
@@ -81,6 +82,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("reopen"))
+@cb_wrapper
 async def reopen_in_cb(_, callback_query: CallbackQuery):
     buttons = paginate_help(0, CMD_HELP, "helpme")
     await app.edit_inline_text(
@@ -91,6 +93,7 @@ async def reopen_in_cb(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("helpme_prev\((.+?)\)"))
+@cb_wrapper
 async def on_plug_prev_in_cb(_, callback_query: CallbackQuery):
     current_page_number = int(callback_query.matches[0].group(1))
     buttons = paginate_help(current_page_number - 1, CMD_HELP, "helpme")
@@ -102,6 +105,7 @@ async def on_plug_prev_in_cb(_, callback_query: CallbackQuery):
 
 
 @app.on_callback_query(filters.regex("helpme_next\((.+?)\)"))
+@cb_wrapper
 async def on_plug_next_in_cb(_, callback_query: CallbackQuery):
     current_page_number = int(callback_query.matches[0].group(1))
     buttons = paginate_help(current_page_number + 1, CMD_HELP, "helpme")
