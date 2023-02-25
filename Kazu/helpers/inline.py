61adfc28bb900d838d70d42dc2255865defbb825
bitchlah <106461653+bitchlah@users.noreply.github.com>
2023-02-25 20:44:23 +0700

    Update inline.py

--- Kazu/helpers/inline.py
+++ Kazu/helpers/inline.py
@@ -54,7 +54,7 @@ def cb_wrapper(func):
         users = list_users
         if cb.from_user.id not in users:
             await cb.answer(
-                "Tidak Semudah Itu Ferguso Yahahaha Hayuukkk",
+                "Maaf, Anda tidak dapat menggunakan Perintah ini! Silahkan Buat Userbot sendiri❣️",
                 cache_time=0,
                 show_alert=True,
             )
