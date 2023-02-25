0ec5b6eab218ed65aad2d22c7753afe441c973af
bitchlah <106461653+bitchlah@users.noreply.github.com>
2023-02-25 21:34:24 +0700

    Update data.py

--- Kazu/helpers/data.py
+++ Kazu/helpers/data.py
@@ -3,10 +3,10 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
 
     text_help_menu = (
-        "**📍 FKM-USERBOT 📍**"
+        "**📍 FKM-USERBOT 📍**\n"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
         .replace("'", "")
     )
-    reopen = [[InlineKeyboardButton("ʀᴇ-ᴏᴘᴇɴ", callback_data="reopen")]]
+    reopen = [[InlineKeyboardButton(text="🗂️ ᴍᴏᴅᴜʟᴇꜱ", callback_data="reopen")], [InlineKeyboardButton(text="☎️ sᴜᴘᴘᴏʀᴛ", url="t.me/ruangdiskusikami"), InlineKeyboardButton(text="📢 ᴜᴘᴅᴀᴛᴇꜱ", url="t.me/ruangprojects")]]
